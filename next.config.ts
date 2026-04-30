824cd117778db67f0cd05c9b4eafd13648c6ace5
Praveen123-png <praveenkumar814444@gmail.com>
2026-04-30 17:04:34 +0530

    htaccess and out folder done

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,28 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+
+  output: "export",
+  trailingSlash: true,
+
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fujisolar.in",
+      },
+    ],
+  },
+
+  experimental: {
+    optimizePackageImports: ["gsap"],
+  },
+
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 };
 
 export default nextConfig;
