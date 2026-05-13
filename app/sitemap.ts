eb26c15a61e11443915f1dcf8b34a683b79ec788
Praveen123-png <praveenkumar814444@gmail.com>
2026-05-13 11:26:10 +0530

    robots and sitemap modified

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,11 +1,13 @@
 import { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
-    { url: "https://yoursite.com", lastModified: new Date() },
-    { url: "https://yoursite.com/about", lastModified: new Date() },
-    { url: "https://yoursite.com/solar-products", lastModified: new Date() },
-    { url: "https://yoursite.com/installation-process", lastModified: new Date() },
-    { url: "https://yoursite.com/contact", lastModified: new Date() },
+    { url: "https://fujisolar.in", lastModified: new Date() },
+    { url: "https://fujisolar.in/about", lastModified: new Date() },
+    { url: "https://fujisolar.in/solar-products", lastModified: new Date() },
+    { url: "https://fujisolar.in/installation-process", lastModified: new Date() },
+    { url: "https://fujisolar.in/contact", lastModified: new Date() },
   ];
 }
